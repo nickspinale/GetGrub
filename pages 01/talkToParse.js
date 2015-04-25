45f47007bc00318f1d3c5642bf2aa4f55362861e
Nick Spinale <spinalen@carleton.edu>
2015-04-24 21:23:10 -0500

    workinonit

--- pages 01/talkToParse.js
+++ pages 01/talkToParse.js
@@ -1,27 +1,47 @@
 Parse.initialize("Jlsk3dPD05eCFHICf8IhS786NsrKR9kP4QNgJ1wL", "8XPomIMiytGITQmvBIZSxYgdslw5RIokTQuHzN1p");
 
-function putPost(request) {
+function isNumber(obj) { return !isNaN(parseFloat(obj)) }
 
-    var Posts = Parse.Object.extend("Posts")
-    var post = new Posts()
+function putPost() {
+	var doLocation = document.getElementById("dropofflocation").value;
+	var puLocation = document.getElementById("pickuplocation").value;
+	var comments = document.getElementById("comments").value;
+	var delFee = document.getElementById("deliveryfee").value;
 
-    console.log("hi")
+	if(!isNumber(delFee)){
+		alert("illegal value for deliveryfee.")
+	} else{
 
-    post.set("username", request.name)
-    post.set("deliveryFee", request.fee)
-    post.set("pickup", request.company)
-    post.set("comments", request.item)
-    post.set("dropoff", request.dropoff)
-    post.set("animalNo", Post.User.current.attribute.userNo)
+        var Posts = Parse.Object.extend("Posts")
+        var post = new Posts()
 
-    post.save(null, {
-        success: function(post) {
-            console.log("DIDIT")
-        },
-        error: function(post, error) {
-            console.log("FUCK")
-        }
-    })
+        var num = "JAVASCRIPTSMD"
+        var q = new Parse.Query(Parse.User)
+        q.equalTo("username", localStorage["username"])
+        q.find().then(function(u) {
+
+            num = u[0].get("userNum")
+
+            console.log("NUMIS" + num)
+
+            post.set("username", localStorage["username"])
+            post.set("deliveryFee", delFee)
+            post.set("pickup", puLocation)
+            post.set("comments", comments)
+            post.set("dropoff", doLocation)
+            post.set("userNum", num)
+
+            post.save(null, {
+                success: function(post) {
+                    window.location = "requestpage.html"
+                    console.log("DIDIT")
+                },
+                error: function(post, error) {
+                    console.log("FUCK")
+                }
+            })
+        })
+	}
 }
 
 // function request(name, animalNo, fee,company, item, dropoff){
@@ -50,7 +70,6 @@ function getPosts() {
                            , item     : results[x].attributes.comments
                            , dropoff  : results[x].attributes.dropoff
                            })
-                console.log(posts[posts.length-1].name);
             }
         },
         error: function(error) {
@@ -62,7 +81,6 @@ function getPosts() {
 }
 
 function login() {
-	//alert("Hi.")
 	var username = document.loginForm.userid.value;
 	var password = document.loginForm.password.value;
 
@@ -75,10 +93,10 @@ function login() {
             console.log(error.message);
         }
     });
+    localStorage["username"] = username
 }
 
 function signup() {
-	//alert("Hi.");
 	var username = document.register.userid.value;
 	var email = document.register.email.value;
 	var password = document.register.password.value;
@@ -96,6 +114,8 @@ function signup() {
 
         var rand = Math.floor((Math.random() * 8) + 1);
         user.set("userNum", rand + "")
+
+        localStorage["username"] = username
 
         user.signUp(null, {
             success: function(user) {
